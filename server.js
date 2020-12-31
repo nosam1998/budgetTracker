0badbbea0732447d995f337d311288cfc65f89f6
nosam1998 <nosam1998@gmail.com>
2020-12-30 19:40:47 -0800

    Production ready!

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
 });
